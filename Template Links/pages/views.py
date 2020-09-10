d433fd7efd4d1545b3ef55d6b79e576e95645637
Nick Kitchel <nkitchel@frontier.com>
2020-09-10 15:51:22 -0600

    We import a template library to help make classes

--- Template Links/pages/views.py
+++ Template Links/pages/views.py
@@ -1,6 +1,9 @@
-# Create your views here.
+#We created our templates here
+#This doesn't use our HttpResponse because we are using templates
 
 from django.views.generic import TemplateView
+
+#We use template classes to create additional pages on our webpage
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
