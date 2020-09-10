9011bd36f84308f4e7ed1da38c2d213d3ccc8ba4
Nick Kitchel <nkitchel@frontier.com>
2020-09-10 17:27:34 -0600

    Edited views.py so we can see our posts from the home page

--- Message Database/posts/views.py
+++ Message Database/posts/views.py
@@ -1,8 +1,9 @@
-#NEW
+#Cleared this file then added everything to this file
 
 from django.views.generic import ListView
 from .models import Post
 
+#This allows us to see the posts from the home page
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
