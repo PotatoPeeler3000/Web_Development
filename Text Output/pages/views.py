605726af7846e1e540352e3867434f5ebff1fe1d
Nick Kitchel <nkitchel@frontier.com>
2020-09-10 15:08:51 -0600

    Stupid editing mistake

--- Text Output/pages/views.py
+++
@@ -1,8 +0,0 @@
-#pages/views/py
-
-from django.http import HttpResponse
-
-# Create your views here.
-
-def homePageView(request):
-    return HttpResponse("Hello Winston! Hope your pong assignemnt works!!")
