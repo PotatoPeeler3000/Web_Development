4b61e9cd3e6475cda20fd13803e80c094bc7214c
Nick Kitchel <nkitchel@frontier.com>
2020-09-10 17:24:53 -0600

    Allows the post message to be displayed on home page

--- Message Database/posts/admin.py
+++ Message Database/posts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Post #NEW
-
-#NEW
-admin.site.register(Post)
+from .models import Post #We add this so we can use our Post class we made from our models file
 
 # Register your models here.
+#This says that when we visit the admin site we will register this Post class
+admin.site.register(Post)
