be1a070305b1cff4cf3899b04820b9b7a78cd79d
Nick Kitchel <nkitchel@frontier.com>
2020-09-10 15:53:16 -0600

    This allows us to view all of our pages on the web

--- Template Links/pages/urls.py
+++ Template Links/pages/urls.py
@@ -1,6 +1,10 @@
+#Made file and added all this
+#We import each of our pages from our views.py file
+
 from django.urls import path
 from .views import HomePageView, AboutPageView, ClothingPageView
 
+#Our urlpatterns adds the link to that path so we can access the page
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name="about"),
