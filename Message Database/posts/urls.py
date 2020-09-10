9c048be124349327a98077c18e31e32a74e589d9
Nick Kitchel <nkitchel@frontier.com>
2020-09-10 17:28:57 -0600

    Now our urls.py should have a homepage ability

--- Message Database/posts/urls.py
+++ Message Database/posts/urls.py
@@ -1,6 +1,8 @@
-#NEW
+#Created this file and added everything to it
 from django.urls import path
 from .views import HomePageView
+
+#This adds the homepageview to our urlpatterns as 'home' in the URL
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
