bf2451c54e42f9ef76a75b47b454602ea1a2e2ef
Nick Kitchel <nkitchel@frontier.com>
2020-09-14 17:01:54 -0600

    Finished rest of blog with simple colors from .css file

--- Blog Database/blog/views.py
+++ Blog Database/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#Cleared this file before using it
+#Then all this was added
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
